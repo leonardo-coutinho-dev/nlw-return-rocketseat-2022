46d9e7850a06c8cba253777afa85212d0b60aecf
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-05 20:53:43 -0300

    adding phosphor react icons to the application

--- trilha-impulse/web-application/src/App.tsx
+++ trilha-impulse/web-application/src/App.tsx
@@ -1,20 +1,5 @@
-interface ButtonProps {
-  text?: string;
-}
+import { Widget } from "./components/Widget";
 
-function Button(props: ButtonProps) {
-  const {text} = props;
-  return <button className="bg-[#8257e6] px-2 h-10 text-neutral-50 hover:bg-violet-700 transition-colors">{text ?? "Default Value"}</button>
+export function App() {
+  return <Widget />;
 }
-
-function App() {
-  return (
-    <div className="div-container">
-      <Button text="texto1"/>
-      <Button text="texto2"/>
-      <Button />
-    </div>
-  )
-}
-
-export default App
---
+++ trilha-impulse/web-application/src/components/Widget.tsx
@@ -0,0 +1,5 @@
+import { Chat, ChatTeardropDots } from 'phosphor-react';
+
+export function Widget() {
+  return <ChatTeardropDots size="200" weight="thin"/>;
+}

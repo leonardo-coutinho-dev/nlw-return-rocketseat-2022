04efc28885bd4c961bc2a434a1e527612f6c2860
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-05 07:57:13 -0300

    tailwind configured, now able to style the application

--- trilha-impulse/web-application/src/App.tsx
+++ trilha-impulse/web-application/src/App.tsx
@@ -4,16 +4,16 @@ interface ButtonProps {
 
 function Button(props: ButtonProps) {
   const {text} = props;
-  return <button>{text ?? "Default Value"}</button>
+  return <button className="bg-blue-500 px-2 h-10 text-neutral-50">{text ?? "Default Value"}</button>
 }
 
 function App() {
   return (
-    <>
+    <div className="div-container">
       <Button text="texto1"/>
       <Button text="texto2"/>
       <Button />
-    </>
+    </div>
   )
 }
 

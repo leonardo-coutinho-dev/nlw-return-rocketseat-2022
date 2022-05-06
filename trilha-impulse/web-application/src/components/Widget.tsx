3c3c1e030678f75eb2f19ee65d4d7574befeebf6
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-06 06:47:08 -0300

    popover implemented

--- trilha-impulse/web-application/src/components/Widget.tsx
+++ trilha-impulse/web-application/src/components/Widget.tsx
@@ -1,22 +1,17 @@
 import { ChatTeardropDots } from 'phosphor-react';
-import { useState } from 'react';
 import { Popover } from '@headlessui/react'
 
 export function Widget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen)
-  }
   return(
-    <div className="absolute bottom-4 right-4">
-      {isWidgetOpen && <div className="h-10 w-10 bg-brand-500 mx-auto mb-2 rounded"></div>}
-      <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+    <Popover className="absolute bottom-4 right-4">
+      <Popover.Panel className="h-10 w-10 bg-brand-500 mx-auto mb-2 rounded"></Popover.Panel>
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-8 h-8"/>
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
           <span className="pl-2"></span>
             feedback
           </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   )
 }

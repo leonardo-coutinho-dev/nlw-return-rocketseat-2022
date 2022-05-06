f83f6074fc4ff0c23181ce6fa04e7a9b2b817d9c
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-06 06:42:21 -0300

    using headless ui lib to improve accessbility

--- trilha-impulse/web-application/src/components/Widget.tsx
+++ trilha-impulse/web-application/src/components/Widget.tsx
@@ -1,5 +1,6 @@
 import { ChatTeardropDots } from 'phosphor-react';
 import { useState } from 'react';
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
   const [isWidgetOpen, setIsWidgetOpen] = useState(false);

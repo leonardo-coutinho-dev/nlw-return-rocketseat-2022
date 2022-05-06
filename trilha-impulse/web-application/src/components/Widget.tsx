92f4e3b9eb6db45707a793b57906b4d8918be10b
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-06 01:26:44 -0300

    Widget component structured, missing only the accessbility part

--- trilha-impulse/web-application/src/components/Widget.tsx
+++ trilha-impulse/web-application/src/components/Widget.tsx
@@ -1,5 +1,21 @@
-import { Chat, ChatTeardropDots } from 'phosphor-react';
+import { ChatTeardropDots } from 'phosphor-react';
+import { useState } from 'react';
 
 export function Widget() {
-  return <ChatTeardropDots size="200" weight="fill"/>;
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen)
+  }
+  return(
+    <div className="absolute bottom-4 right-4">
+      {isWidgetOpen && <div className="h-10 w-10 bg-brand-500 mx-auto mb-2 rounded"></div>}
+      <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+        <ChatTeardropDots className="w-8 h-8"/>
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2"></span>
+            feedback
+          </span>
+      </button>
+    </div>
+  )
 }

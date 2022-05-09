33112ed0edb718cf4e721ee6bd6597bc65881928
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-09 13:48:08 -0300

    componentizando a aplicação

--- trilha-impulse/web-application/src/components/WidgetForm/index.tsx
+++ trilha-impulse/web-application/src/components/WidgetForm/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
-import { CloseButton } from "./CloseButton";
+import { CloseButton } from "../CloseButton";
 
-import bugImageURL from './../assets/bug.svg';
-import ideaImageURL from './../assets/idea.svg';
-import thoughtImageURL from './../assets/thought.svg';
+import bugImageURL from '../../assets/bug.svg';
+import ideaImageURL from '../../assets/idea.svg';
+import thoughtImageURL from '../../assets/thought.svg';
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
 /**
  * Object.entries(feedbackTypes) =>
@@ -15,7 +16,7 @@ import thoughtImageURL from './../assets/thought.svg';
   * ]
  */
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: 'Problema',
     image: {
@@ -39,11 +40,11 @@ const feedbackTypes = {
   },
 };
 
-type feedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm () {
 
-  const [feedbackType, setFeedbackType] = useState<feedbackType | null>(null);
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -51,21 +52,11 @@ export function WidgetForm () {
         <span className="text-xl leading-6">Deixe seu feedback</span>
         <CloseButton />
       </header>
-      <section className="flex justify-between py-4 gap-2 w-full">
-        { Object.entries(feedbackTypes).map(([key, value]) => {
-          return(
-            <button
-              key={key}
-              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-              onClick={() => setFeedbackType(key as feedbackType)}
-              type="button"
-            >
-              <img src={value.image.source} alt={value.image.alt} />
-              <span>{value.title}</span>
-            </button>
-          );
-        }) }
-      </section>
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+      ) : (
+        <p>Hello World!</p>
+      )}
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela <a className="underline underline-offset-2" href="https://www.rocketseat.com.br" target="_blank">Rocketseat</a>
       </footer>

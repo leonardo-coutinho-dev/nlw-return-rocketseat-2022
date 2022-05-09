9f4981f2f9baee71879e9c3b32694dc6d9765e26
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-08 21:50:13 -0300

    creating popover pannel widget form

--- trilha-impulse/web-application/src/components/WidgetForm.tsx
+++ trilha-impulse/web-application/src/components/WidgetForm.tsx
@@ -1,5 +1,9 @@
 import { CloseButton } from "./CloseButton";
 
+import {Cube} from 'phosphor-react'
+
+const text = ['text-1', 'text-2', 'text-3'];
+
 export function WidgetForm () {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-80">
@@ -7,10 +11,19 @@ export function WidgetForm () {
         <span className="text-xl leading-6">Deixe seu feedback</span>
         <CloseButton />
       </header>
-      <section className="flex py-4 gap-2 w-full">
-        <div className="w-1/3 h-20 bg-slate-100"></div>
-        <div className="w-1/3 h-20 bg-slate-100"></div>
-        <div className="w-1/3 h-20 bg-slate-100"></div>
+      <section className="flex py-4 gap-2 w-full text-[#000000]">
+        <div className="w-1/3 h-20 bg-slate-100 flex flex-col items-center justify-center">
+          <h2>{text[0]}</h2>
+          <Cube className="w-8 h-8" />
+        </div>
+        <div className="w-1/3 h-20 bg-slate-100 flex flex-col items-center justify-center">
+          <h2>{text[1]}</h2>
+          <Cube className="w-8 h-8" />
+        </div>
+        <div className="w-1/3 h-20 bg-slate-100 flex flex-col items-center justify-center">
+          <h2>{text[2]}</h2>
+          <Cube className="w-8 h-8" />
+        </div>
       </section>
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela <a className="underline underline-offset-2" href="https://www.rocketseat.com.br" target="_blank">Rocketseat</a>

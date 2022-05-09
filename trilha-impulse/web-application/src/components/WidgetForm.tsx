289c5085ce5ed89bf51f3f85b3d1f5b3e2349bf8
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-09 01:57:28 -0300

    tipagem com typescript, criando useState para armazenar em qual tipo de feedback o usuário clicou

--- trilha-impulse/web-application/src/components/WidgetForm.tsx
+++ trilha-impulse/web-application/src/components/WidgetForm.tsx
@@ -1,29 +1,70 @@
+import { useState } from "react";
+
 import { CloseButton } from "./CloseButton";
 
-import {Cube} from 'phosphor-react'
+import bugImageURL from './../assets/bug.svg';
+import ideaImageURL from './../assets/idea.svg';
+import thoughtImageURL from './../assets/thought.svg';
 
-const text = ['text-1', 'text-2', 'text-3'];
+/**
+ * Object.entries(feedbackTypes) =>
+  * [
+  *  ['BUG', {...}],
+  *  ['IDEA', {...}],
+  *  ['THOUGHT', {...}],
+  * ]
+ */
+
+const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageURL,
+      alt: 'Imagem de um inseto',
+    }
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageURL,
+      alt: 'Imagem de uma lâmpada',
+    }
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageURL,
+      alt: 'Imagem de um balão de pensamento',
+    }
+  },
+};
+
+type feedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm () {
+
+  const [feedbackType, setFeedbackType] = useState<feedbackType | null>(null);
+
   return (
-    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-80">
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
         <span className="text-xl leading-6">Deixe seu feedback</span>
         <CloseButton />
       </header>
-      <section className="flex py-4 gap-2 w-full text-[#000000]">
-        <div className="w-1/3 h-20 bg-slate-100 flex flex-col items-center justify-center">
-          <h2>{text[0]}</h2>
-          <Cube className="w-8 h-8" />
-        </div>
-        <div className="w-1/3 h-20 bg-slate-100 flex flex-col items-center justify-center">
-          <h2>{text[1]}</h2>
-          <Cube className="w-8 h-8" />
-        </div>
-        <div className="w-1/3 h-20 bg-slate-100 flex flex-col items-center justify-center">
-          <h2>{text[2]}</h2>
-          <Cube className="w-8 h-8" />
-        </div>
+      <section className="flex justify-between py-4 gap-2 w-full">
+        { Object.entries(feedbackTypes).map(([key, value]) => {
+          return(
+            <button
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              onClick={() => setFeedbackType(key as feedbackType)}
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        }) }
       </section>
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela <a className="underline underline-offset-2" href="https://www.rocketseat.com.br" target="_blank">Rocketseat</a>

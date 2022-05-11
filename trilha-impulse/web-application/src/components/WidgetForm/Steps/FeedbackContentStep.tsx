038038fb9768fb6cee9d33f92629ff9bf7bcad85
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-11 03:20:57 -0300

    content step component styles finalized

--- trilha-impulse/web-application/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ trilha-impulse/web-application/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,26 +1,46 @@
+import { ArrowLeft } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
-import { BackButton } from "../../BackButton";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
 }
 
-export function FeedbackContentStep ({ feedbackType }: FeedbackContentStepProps) {
+export function FeedbackContentStep ({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
   const feedbackTypeInfo = feedbackTypes[feedbackType];
   return (
     <>
       <header>
-        <BackButton />
-        <div className="flex flex-row">
+        <button 
+          type="button" 
+          className="absolute top-5 left-5 text-zinc-400 hover:text-zinc-100"
+          onClick={onFeedbackRestartRequested}
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+        </button>
+        <span className="text-xl leading-6 flex items-center gap-2">
           <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6"/>
-          <span className="text-xl leading-6">{feedbackTypeInfo.title}</span>
-        </div>
+          {feedbackTypeInfo.title}
+          </span>
         <CloseButton />
       </header>
-      <section className="flex justify-between py-8 gap-2 w-full">
-        
-      </section>
+      <form className="my-4 w-full">
+        <textarea 
+          className="min-w-[304px] w-full min-h-[102px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-transparent"
+          placeholder="Conte-nos com detalhes o que está acontecendo!"
+        />
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton />
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar feedback
+          </button>
+        </footer>
+      </form>
     </>
   )
 }
--- trilha-impulse/web-application/src/components/WidgetForm/index.tsx
+++ trilha-impulse/web-application/src/components/WidgetForm/index.tsx
@@ -45,12 +45,19 @@ export function WidgetForm () {
 
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
+  function handleRestartFeedback () {
+    setFeedbackType(null);
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
       ) : (
-        <FeedbackContentStep feedbackType={feedbackType}/>
+        <FeedbackContentStep 
+          feedbackType={feedbackType}
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
       )}
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela <a className="underline underline-offset-2" href="https://www.rocketseat.com.br" target="_blank">Rocketseat</a>

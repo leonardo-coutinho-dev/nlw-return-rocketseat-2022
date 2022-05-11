de82a9398e353681c1043b6dea7e6e4d11074e0a
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-11 02:03:22 -0300

    adding content component

--- trilha-impulse/web-application/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ trilha-impulse/web-application/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,26 @@
-export function FeedbackContentStep () {
-  return(
-    <h1 className="text-yellow-400">Feedback Content Component</h1>
+import { FeedbackType, feedbackTypes } from "..";
+import { BackButton } from "../../BackButton";
+import { CloseButton } from "../../CloseButton";
+
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+}
+
+export function FeedbackContentStep ({ feedbackType }: FeedbackContentStepProps) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+  return (
+    <>
+      <header>
+        <BackButton />
+        <div className="flex flex-row">
+          <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6"/>
+          <span className="text-xl leading-6">{feedbackTypeInfo.title}</span>
+        </div>
+        <CloseButton />
+      </header>
+      <section className="flex justify-between py-8 gap-2 w-full">
+        
+      </section>
+    </>
   )
 }

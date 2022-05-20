8b8b00f0ec42fecdd1a517b80f5fa909562eab49
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-20 09:46:58 -0300

    creating feedbacks route

--- trilha-impulse/web-application/server/src/server.ts
+++ trilha-impulse/web-application/server/src/server.ts
@@ -2,7 +2,22 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (req, res) => {
+// MÉTODOS HTTP: GET, POST, PUT, PATCH, DELETE, ...
+
+/** 
+
+GET = Buscar informações
+POST = Cadastrar informações
+PUT = Atualizar informações de uma entidade (Por exemplo, imagine uma rota de alteração de perfil ~ nome, e-mail, senha, foto de perfil, ... ~ alterando uma entidade completa)
+PATCH = Atualizar uma informação única de uma entidade (Por exemplo, escolher se você quer receber ou não a newsletter de uma plataforma ~ alteração de um valor específico da entidade)
+DELETE = Deletar uma informação
+
+Método = (HTTP)
+Recurso = O que estamos criando, atualizando, deletando, ...
+ 
+*/
+
+app.post('/feedbacks', (req, res) => {
   return res.send('Hey, what do you need boy?')
 })
 

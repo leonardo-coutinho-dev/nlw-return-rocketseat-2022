6e0bd2190a2f8e6d797240ec6e8481ccc059895b
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-25 03:07:22 -0300

    aplicando o SOLID na aplicação

--- trilha-impulse/web-application/server/src/server.ts
+++ trilha-impulse/web-application/server/src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
-import nodemailer from 'nodemailer';
-import { prisma } from './prisma';
+
+import { routes } from './routes';
 
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('HTTP Server running');
+});
 
 // MÉTODOS HTTP: GET, POST, PUT, PATCH, DELETE, ...
 
@@ -21,39 +26,3 @@ Método = (HTTP)
 Recurso = O que estamos criando, atualizando, deletando, ...
  
 */
-
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "2c68297fa4fe33",
-    pass: "44b9386435b54d"
-  }
-});
-
-app.post('/feedbacks', async (req, res) => {
-  const { type, comment, screenshot } = req.body;
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    }
-  })
-  await transport.sendMail({
-    from: 'Equipe Feedget <oi@feedget.com>',
-    to: 'Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>',
-    subject: 'Novo feedback',
-    html: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      `</div>`
-    ].join('\n')
-  })
-  return res.status(201).json({ data: feedback })
-});
-
-app.listen(3333, () => {
-  console.log('HTTP Server running');
-});

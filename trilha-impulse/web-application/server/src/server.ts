73f3a3aba087985b72e2345b00238f1342291422
Leonardo Coutinho dos Santos <leonardo.coutinho@vetta.digital>
2022-05-25 01:57:10 -0300

    configurando o back-end da aplicação, banco de dados, inserção de informações no banco de dados e envio de e-mail com essas informações

--- trilha-impulse/web-application/server/src/server.ts
+++ trilha-impulse/web-application/server/src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+import { prisma } from './prisma';
+
 
 const app = express();
+
+app.use(express.json());
 
 // MÉTODOS HTTP: GET, POST, PUT, PATCH, DELETE, ...
 
@@ -17,9 +22,37 @@ Recurso = O que estamos criando, atualizando, deletando, ...
  
 */
 
-app.post('/feedbacks', (req, res) => {
-  return res.send('Hey, what do you need boy?')
-})
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "2c68297fa4fe33",
+    pass: "44b9386435b54d"
+  }
+});
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
+  })
+  return res.status(201).json({ data: feedback })
+});
 
 app.listen(3333, () => {
   console.log('HTTP Server running');
